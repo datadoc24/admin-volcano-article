15f257ad2144f96f0f1c779490f31d7494410d50
datadoc24 <abe24seven@gmail.com>
2024-11-22 11:41:52 -0800

    Update create-do-k8s.sh

--- create-do-k8s.sh
+++ create-do-k8s.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+#Export your Digital Ocean Personal Access Token (with all scopes enabled) as DO_PAT
+#for example
+#export DO_PAT=do_v1_xxxxxxx
+
+if [ -z "${DO_PAT}" ]; then
+    echo "Please export your Digital Ocean PAT as DO_PAT and retry"
+    exit 1
+fi
+
 tofu init
-tofu plan
-tofu apply -auto-approve
+tofu plan -var "do_token=${DO_PAT}"
+tofu apply -var "do_token=${DO_PAT}" -auto-approve
 tofu output -raw kubeconfig > ~/.kube/dok8s-config
+
 export APISERVER=$(tofu output -raw apiserver)
-export KUBECONFIG="/home/abe/.kube/dok8s-config"
+export KUBECONFIG="~/.kube/dok8s-config"
+echo -e "API server: $APISERVER\nKUBECONFIG location: $KUBECONFIG"
